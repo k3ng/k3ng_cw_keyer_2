9e03c1afd5455570d46421830f9fa27c059d9655
Anthony Good <anthonygood@Anthonys-MacBook-Pro-2.local>
2026-06-26 15:43:05 -0400

    Move FEATURE_POTENTIOMETER to active features block

--- k3ng_keyer_2/keyer_2_features_and_options.h
+++ k3ng_keyer_2/keyer_2_features_and_options.h
@@ -33,16 +33,14 @@
 #define FEATURE_BUTTONS                                   // Analog multiplexed button array (R1/R2 resistor ladder on analog_buttons_pin)
 #define FEATURE_MEMORIES                                  // CW memory storage/playback via EEPROM (buttons 1-3, \1-\3, command mode P)
 #define FEATURE_MEMORY_MACROS                             // Backslash macro commands within memories (\S \E \C \N \W \Y \Z \D \U \V \T \F \I \0-\9)
-
+#define FEATURE_POTENTIOMETER                     // Speed control potentiometer (do not enable without hardware connected)
 
 // *** Not implemented yet ***
 
 // --- Features ---
-
 // #define FEATURE_COMMAND_LINE_INTERFACE_ON_SECONDARY_PORT  // CLI on a secondary serial port
 // #define FEATURE_BEACON                         // Beacon mode if paddle_left is LOW at boot
 // #define FEATURE_BEACON_SETTING                 // Beacon mode controlled by EEPROM setting (\_ CLI command)
-#define FEATURE_POTENTIOMETER                     // Speed control potentiometer (do not enable without hardware connected)
 // #define FEATURE_SIDETONE_SWITCH                // External toggle switch for sidetone on/off
 // #define FEATURE_SIDETONE_NEWTONE               // Use NewTone library instead of standard tone() (~1k smaller; timer1, pins 9/10)
 // #define FEATURE_AUTOSPACE                      // Automatic character spacing
